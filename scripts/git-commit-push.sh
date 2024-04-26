95bc6497160a596453cd6971b0ffd8a15b16c4b2
Michael Barros <michaelcbarros@gmail.com>
2024-04-26 17:49:38 -0400

    2024-04-26 17:49:38

--- scripts/git-commit-push.sh
+++ scripts/git-commit-push.sh
@@ -1,20 +1,11 @@
 # Get current TIMESTAMP
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Extract version from package.json
-VERSION=$(jq -r .version package.json)
-
 # Add all changes to the git index
 git add .
 
 # Commit the changes
 git commit -m "$TIMESTAMP"
 
-# Tag the current commit with the version
-git tag -f "v$VERSION"
-
 # Push the repo to the remote repository
 git push origin master
-
-# Push the tag to the remote repository
-git push origin "v$VERSION"
