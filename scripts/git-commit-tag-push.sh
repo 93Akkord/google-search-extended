85118931de2cb3c612474da51b564f98226c55e5
Michael Barros <michaelcbarros@gmail.com>
2024-04-26 18:02:11 -0400

    2024-04-26 18:02:11

--- scripts/git-commit-tag-push.sh
+++ scripts/git-commit-tag-push.sh
@@ -10,11 +10,11 @@ git add .
 # Commit the changes
 git commit -m "$TIMESTAMP"
 
-# Tag the current commit with the version
-git tag -f "v$VERSION"
-
 # Push the repo to the remote repository
 git push origin master
+
+# Tag the current commit with the version
+git tag -f "v$VERSION"
 
 # Push the tag to the remote repository
 git push origin "v$VERSION"
--- src/google-search-extended.user.js
+++ src/google-search-extended.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        google-search-extended
 // @namespace   https://openuserjs.org/users/93Akkord
-// @version     0.2.18
+// @version     0.2.19
 // @description Google Search Extended
 // @license     MIT
 // @author      93Akkord
